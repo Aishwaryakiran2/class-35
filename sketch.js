1eaaa5799db7fe93d79236a9b9aa9331937c391c
Aishwaryakiran2 <92712139+Aishwaryakiran2@users.noreply.github.com>
2022-02-04 06:11:36 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,58 @@
+var rball;
+var database;
+var position;
+function setup(){
+    database = firebase.database();
+    createCanvas(500,500);
+    rball = createSprite(250,250,10,10);
+    rball.shapeColor = "red";
+
+    var rballPosition = database.ref('ball/position')
+    rballPosition.on("value",readPosition,showError)
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        changePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        changePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        changePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        changePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function changePosition(x,y){
+    
+database.ref('ball/position').set({
+'x':position.x+x,
+'y':position.y+y,
+})
+
+
+
+
+}
+
+function readPosition(data){
+position = data.val();
+rball.x=position.x;
+rball.y=position.y;
+    
+}
+
+function showError(){
+console.log("error in wirtting database")
+
+}
+
+
+
+
+
